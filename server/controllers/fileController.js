7acc7775eef382549462849886004cf9e5b72e37
Jwookie55 <jwookie55@live.com>
2016-09-15 18:43:51 -0700

    merge

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -1,14 +1,14 @@
+const Promise = require('bluebird');
+const MongoClient = require('mongodb').MongoClient;
+
 const File = require('./../models/fileModel');
 const User = require('./../models/userModel');
-const MongoClient = require('mongodb').MongoClient;
 
 const fileController = {};
 
 fileController.createFile = (req, res) => {
   const file = new File(req.body);
-
   // find object with correct username in database, create a file schema & push into files array
-
   User.findByIdAndUpdate(req.cookies.id, { $push: { files: file._id } }, (err) => console.log(err));
   file.save((err) => {
     if (err) {
@@ -22,17 +22,33 @@ fileController.createFile = (req, res) => {
   });
 };
 
-fileController.getFile = (req, res, next) => {
-  File.findById(req.cookies.id, (err, docs) => {
+fileController.getUserFiles = (req, res, next) => {
+  User.findById(req.cookies.id, (err, docs) => {
     if (err) {
       throw err;
     } else {
       // do something to load file names into dropdown
-      res.files = docs;
-      console.log('res.files: ', res.files);
+      res.userFiles = docs.files;
       next();
     }
   });
+};
+
+fileController.parseFiles = (req, res, next) => {
+  const allFiles = [];
+  const promises = [];
+  for (let i = 0; i < res.userFiles.length; i++) {
+    promises.push(
+    new Promise((resolve, reject) => {
+      File.findById(res.userFiles[i], (err, file) => {
+        if (err) {
+          console.log('file missing, id: ', res.userFile[i]);
+        }
+        resolve(allFiles.push(file));
+      });
+    }));
+  }
+  Promise.all(promises).then(() => { res.files = allFiles; next(); });
 };
 
 module.exports = fileController;
